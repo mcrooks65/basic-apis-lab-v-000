35e79be9abbc4238bbb834426224c4af990574e0
Matthew Crooks <mfcrooks@gmail.com>
2018-10-16 22:19:08 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -10,6 +10,7 @@ class RepositoriesController < ApplicationController
         req.params["q"] = params[:query] 
         req.params["client_id"] = 'be9ee16ec39bdfdbcd7f'
         req.params["client_secret"] = 'a3b8aace511b332997dd9c31ae516cfffe2b83f7'
+      end
     end
     
     render 'search'
